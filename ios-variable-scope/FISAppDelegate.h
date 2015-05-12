f7b4cc9482d88ebb9a85edc748995f463a512f86
Tim Clem <tim.clem@gmail.com>
2015-05-12 10:30:51 -0400

    Initial Commit

---
+++ ios-variable-scope/FISAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FISAppDelegate.h
+//  ios-variable-scope
+//
+//  Created by iOS Staff on 5/12/15
+//  Copyright (c) 2015 The Flatiron School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FISAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ios-variable-scope/Resources/Other-Sources/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ios-variable-scope
+//
+//  Created by iOS Staff on 5/12/15
+//  Copyright (c) 2015 The Flatiron School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "FISAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FISAppDelegate class]));
+    }
+}
