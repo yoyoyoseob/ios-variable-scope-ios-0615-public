785418eb45aa2fa59852408a63143ec3e821a194
yoyoyoseob <yoyoyoseob@gmail.com>
2015-05-26 13:01:16 -0400

    Add 3 methods for tests

--- ios-variable-scope/FISAppDelegate.m
+++ ios-variable-scope/FISAppDelegate.m
@@ -16,9 +16,40 @@
 @implementation FISAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-
-
     return YES;
 }
+
+- (NSMutableArray *)arrayByAddingString:(NSString *)string toArray:(NSMutableArray *)array
+{
+    NSMutableArray *newArray = [[NSMutableArray alloc] initWithArray:array];
+    [newArray addObject:string];
+    return newArray;
+}
+
+- (NSUInteger)countOfStringsInAllCapsInArray:(NSArray *)array;
+{
+    NSUInteger count = 0;
+    for (NSString *word in array){
+        if ([word isEqualToString:[word uppercaseString]]){
+            count += 1;
+        }
+    }
+    return count;
+}
+
+- (void)removeAllElementsFromArray:(NSMutableArray *)array
+{
+    [array removeAllObjects];
+}
+
+// Pull object from index: i and assign to variable {}
+// NSString *wordTester = [array objectAtIndex:i];
+
+// Remove all accents and reassign to new variable
+// NSString* wordTested = [[wordTester componentsSeparatedByCharactersInSet:[[NSCharacterSet letterCharacterSet] invertedSet]] componentsJoinedByString:@""];
+
+// Add new updated objects to new array for counter
+// Add +1 to counter for each after same test from above
+
 
 @end
